44df6036b291bb6e62af7348d68729115ef1bd07
Namy <damienbiron33@gmail.com>
2023-12-13 16:31:54 +0100

    react01

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,21 @@ function App() {
  
  return (
    <div>
-    <PokemonCard/>
+    <PokemonCard pokemon={pokemonList[0]}/>
    </div>
   )
 }
+
+const pokemonList = [
+  {
+    name: "balbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 
 export default App
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,30 +1,14 @@
 
-const pokemonList = [
-  {
-    name: "balbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
-
-
-
-
-function PokemonCard() {
-    const pokemon = pokemonList[0]
-    
+function PokemonCard(props) {
+  const {pokemon} = props
     return (
         <figure>
-            {pokemon.imgSrc ? (<img src={pokemon.imgSrc}alt={pokemon.name}></img>): (<p>???</p>)}
-            <figcaption >{pokemon.name}</figcaption>
+            {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name}/>): (<p>???</p>)}
+            <figcaption>{pokemon.name}</figcaption>
             </figure>
-    )
+    );
   
   }
 
 
-  export default PokemonCard; pokemonList;
+  export default PokemonCard; 
